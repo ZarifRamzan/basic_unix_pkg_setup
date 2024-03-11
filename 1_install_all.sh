d88e75a8e35927cd1506328b8a47756fd618d196
Zarif <mohamadzarif96@gmail.com>
2024-03-12 06:32:31 +0800

    Update 1_install_all.sh

--- 1_install_all.sh
+++ 1_install_all.sh
@@ -2,20 +2,14 @@
 
 export DIR=$(pwd)
 
-chmod 775 $DIR/2_install_unix_packages.sh
-./2_install_unix_packages.sh  
+sudo apt-get -y update
+sudo apt-get -y upgrade
 
 chmod 775 $DIR/3_bashrc_setup.sh
 ./3_bashrc_setup.sh 
 
 chmod 775 $DIR/4_vimrc_setup.sh
 ./4_vimrc_setup.sh  
-
-chmod 775 $DIR/5_install_nvim.sh
-./5_install_nvim.sh
-
-chmod 775 $DIR/6_wsl2_kali_kex.sh
-./6_wsl2_kali_kex.sh
 
 chmod 775 $DIR/7_inputrc_setup
 ./7_inputrc_setup
